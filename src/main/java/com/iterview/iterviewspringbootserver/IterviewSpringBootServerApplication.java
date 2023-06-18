24d0372f98d9d50b507ca8b9fceeed05318bc83d
imygnam <imygnam@gmail.com>
2023-06-18 17:37:30 +0900

    [FEAT] base

---
+++ src/main/java/com/iterview/iterviewspringbootserver/IterviewSpringBootServerApplication.java
@@ -0,0 +1,13 @@
+package com.iterview.iterviewspringbootserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IterviewSpringBootServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IterviewSpringBootServerApplication.class, args);
+    }
+
+}
